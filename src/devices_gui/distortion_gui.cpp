6179a6ce48f1fbcca5bbbf59a709649d4a6b46b7
jraskell1 <jraskell1@gmail.com>
2014-12-02 14:20:48 -0500

    Start of Distortion Filter GUI
    Creating a simple GUI for the Distortion Filter along with a 'minimal'
    test project.

---
+++ src/devices_gui/distortion_gui.cpp
@@ -0,0 +1,47 @@
+#include "distortion_gui.h"
+
+DistortionGUI::DistortionGUI(wxWindow* parent, wxWindowID id,
+	const wxPoint& pos, DistortionFilter* device)
+	: wxPanel(parent, id, pos, wxSize(200, 100), wxSTATIC_BORDER, wxT("Distortion Filter"))
+{
+	if (device == nullptr){
+		filter = new DistortionFilter("Distortion Filter");
+	}
+	else{
+		filter = device;
+	}
+
+
+	//Create 'wrap' for Distortion GUI element
+	wrap = new wxStaticBox(this, wxID_ANY, wxT("Distortion Filter"));
+
+
+	//Set up the sizer for the panel
+	sizer = new wxStaticBoxSizer(wrap, wxVERTICAL);
+
+
+	//Create the enabled/disabled checkbox
+	chkEnabled = new wxCheckBox(this, wxID_ANY, wxT("&Enabled"), wxDefaultPosition, wxDefaultSize);
+	chkEnabled->SetValue(true);
+	sizer->Add(chkEnabled);
+	//Create the 'Drive' slider input for the distortion level
+	DriveControl = new wxSlider(this, wxID_ANY, 20, 10, 40, wxDefaultPosition, wxDefaultSize, wxSL_VERTICAL | wxSL_LABELS);
+	sizer->Add(DriveControl, 0, wxALIGN_CENTER, 5);
+
+	SetSizer(sizer);
+	sizer->Fit(this);
+	sizer->SetSizeHints(this);
+}
+
+AbstractAudio* DistortionGUI::getDevice()
+{
+	return filter;
+}
+
+
+void DistortionGUI::setDevice(DistortionFilter* device)
+{
+	if (device != nullptr){
+		filter = device;
+	}
+}
---
+++ src/devices_gui/distortion_gui.h
@@ -0,0 +1,28 @@
+#ifndef DISTORTION_GUI_H
+#define DISTORTION_GUI_H
+
+#include "wx/wx.h"
+#include "wx/checkbox.h"
+#include "wx/slider.h"
+#include "wx/sizer.h"
+#include "devices/filters/DistortionFilter.h"
+
+
+class DistortionGUI : public wxPanel
+{
+public:
+	DistortionGUI(wxWindow* parent, wxWindowID id, 
+		const wxPoint& pos = wxDefaultPosition, DistortionFilter* device = nullptr);
+
+	AbstractAudio* getDevice();
+	void setDevice(DistortionFilter* device);
+
+private:
+	DistortionFilter* filter;
+	wxStaticBoxSizer* sizer;
+	wxStaticBox* wrap;
+	wxCheckBox* chkEnabled;
+	wxSlider* DriveControl;
+};
+
+#endif
