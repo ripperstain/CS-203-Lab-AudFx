c3e906726dc8af87f8f8d3a2e9c7d26fd51ff19f
Jeremy Sayers <Jsayers@student.dwc.edu>
2015-01-20 13:54:46 -0500

    Purged the KaraokeProcessor of already implemented functions.

--- src/devices/KaraokeProcessor.cpp
+++ src/devices/KaraokeProcessor.cpp
@@ -12,39 +12,12 @@ int KaraokeProcessor::getSamples(float* buffer, int length){
 
 	//Check to see how many channels there are
 	if (Enabled == true && NumChannels == 2) {
-		//Checks to see if the audio is 8 bit
-		//if (previous->getAudioFormat().BitsPerSample == 8) {
-			//Subtracts Right from Left, store in left, and vice versa
-			//Then multiplying by .8 to adjust amplitude
 			for (int i = 0; i < samplesReturned; i += 2){
 				float tempSample = buffer[i];
 				buffer[i] = (buffer[i] - buffer[i + 1]) / .8;
 				buffer[i + 1] = (buffer[i + 1] - tempSample) / .8;
 			}
-		//}
-		//Checks to see if the audio is 16 bit
-		//else if (previous->getAudioFormat().BitsPerSample == 16) {
-			//Subtracts Right from Left, store in left, and vice versa
-			//Then multiplying by .8 to adjust amplitude
-			//for (int i = 0; i < samplesReturned; i += 4){
-			//	char tempSample = buffer[i];
-			//	char tempSample2 = buffer[i + 1];
-			//	buffer[i] = (buffer[i] - buffer[i + 2]) * .8;
-			//	buffer[i + 1] = (buffer[i + 1] - buffer[i + 3]) * .8;
-			//	buffer[i + 2] = (buffer[i+2] - tempSample) * .8;
-			//	buffer[i + 3] = (buffer[i + 3] - tempSample2) * .8;
-			//}
-		//}
+
 	}
 	return samplesReturned;
-}
-
-void KaraokeProcessor::setNext(AbstractAudio *n)
-{
-	this->next = n;
-}
-
-void KaraokeProcessor::setPrevious(AbstractAudio *p)
-{
-	this->previous = p;
 }
